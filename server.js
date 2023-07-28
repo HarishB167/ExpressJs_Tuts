1341809e66207207b67f2cf110b752dcb9c4d8a3
Harish <harishbb168@gmail.com>
2023-07-28 15:22:48 +0530

    Using mongodb, mongoose

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require("express");
+require("dotenv").config();
 
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
 app.set("view engine", "ejs");
 
@@ -11,5 +18,4 @@ app.get("/", (req, res) => {
 const userRouter = require("./routes/users");
 app.use("/users", userRouter);
 
-app.listen(3000);
-console.log("Server started...");
+app.listen(3000, () => console.log("Server started..."));
