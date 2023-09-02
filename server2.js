fc4cec7b904b726e2a74aac646136cc9a481ec38
Harish <harishpoctor@gmail.com>
2023-09-02 08:12:44 +0000

    JWT authentication - refresh tokens completed.

--- server2.js
+++ server2.js
@@ -20,16 +20,6 @@ app.get("/posts", authenticateToken, (req, res) => {
   res.json(posts.filter((p) => p.username == req.user.name));
 });
 
-app.post("/login", (req, res) => {
-  // Authenticate user
-
-  const username = req.body.username;
-  const user = { name: username };
-
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken });
-});
-
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
